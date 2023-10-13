cbd3dba4d96d0ee93f6b102b30572035a4f44abc
SofiaGL082 <sogiraldo@unal.edu.co>
2023-10-12 22:34:34 -0500

    :apple:

--- src/test/java/futbol/Futbolista.java
+++ src/test/java/futbol/Futbolista.java
@@ -49,14 +49,12 @@ public abstract class Futbolista implements Comparable<Futbolista> {
 		return respuesta;
 	}
 	
+	@Override
 	public int compareTo(Futbolista f) {
-		if (this.getEdad()>f.getEdad()) {
-			return 1;
-		}else if (this.getEdad()<f.getEdad()) {
-			return -1;
-		}else {
-			return 0;
-		}
+	    if (this.getEdad() != f.getEdad()) {
+	        return this.getEdad() - f.getEdad();
+	    }
+	    return this.getNombre().compareTo(f.getNombre());
 	}
 	
 	public abstract boolean jugarConLasManos();
